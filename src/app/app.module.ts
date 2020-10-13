142141c21bdd97287b0b7fa4b0f497d668840c68
Carlos Franco <cafranco0505@gmail.com>
2020-10-12 21:07:39 -0500

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TopbannerComponent } from './components/topbanner/topbanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TopbannerComponent
   ],
   imports: [
     BrowserModule,
