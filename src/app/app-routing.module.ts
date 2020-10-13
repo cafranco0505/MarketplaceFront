d515c51922107276980af2a8c2bbbdbcbfc584ca
Carlos Franco <cafranco0505@gmail.com>
2020-10-13 00:40:55 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: BodyComponent},
+  {path: 'inicia-sesion', component: SignInComponent},
+  {path: 'registro', component: SignUpComponent},
+  {path: 'planes', component: PricingComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
