e070dcacdeed05d2226763913e12f4a77ab020b1
Carlos Franco <cafranco0505@gmail.com>
2020-10-12 22:52:11 -0500

    options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TopbannerComponent } from './components/topbanner/topbanner.component';
 import { FeaturesComponent } from './components/features/features.component';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     TopbannerComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
