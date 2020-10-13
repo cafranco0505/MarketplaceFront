04bddd8bad167fd2074a2557f03f1ed371d20640
Carlos Franco <cafranco0505@gmail.com>
2020-10-13 10:09:20 -0500

    front ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { BodyComponent } from './components/body/body.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { PricingComponent } from './components/pricing/pricing.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path: 'inicio', component: BodyComponent},
   {path: 'inicia-sesion', component: SignInComponent},
   {path: 'registro', component: SignUpComponent},
   {path: 'planes', component: PricingComponent},
+  {path: 'about', component: AboutComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
 ];
 
